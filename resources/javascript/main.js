9564af094630152ead873c177a7f49ada583e6c0
JamesLDeveloper <jameslloyd2007@hotmail.co.uk>
2024-08-20 23:18:28 +0100

    add js to change images when pressing down on them and revert back when not pressing down on them. Add about me section, including images, projects, skills, footer, contact details etc. Style all parts as necessary

---
+++ resources/javascript/main.js
@@ -0,0 +1,56 @@
+/*const imagesOne = ["What-I-Think-I-Look-Like-Image"];
+
+const imagesTwo = ["What-I-Actually-Look-Like-Image"];
+
+const imagesOneArray = [];
+
+imagesOne.forEach(function (image) {
+    imagesOneArray.push(document.getElementById(image));
+});
+
+const imagesTwoArray = [];
+
+imagesTwo.forEach(function (image) {
+    imagesTwoArray.push(document.getElementById(image));
+});
+
+imagesOneArray.forEach(function (image) {
+    image.addEventListener("mousedown", changeImageOne);
+    image.addEventListener("mouseup", revertImageOne);
+});
+
+imagesTwoArray.forEach(function (image) {
+    image.addEventListener("mousedown", changeImageTwo);
+    image.addEventListener("mouseup", revertImageTwo);
+});
+*/
+
+const imageToChangeOne = document.getElementById("What-I-Think-I-Look-Like-Image");
+
+const imageToChangeTwo = document.getElementById("What-I-Actually-Look-Like-Image");
+
+const changeImageOne = (event) => {
+event.target.style.backgroundImage = "url('resources/img/Superman-Flying/res-mipmap/mipmap-xxxhdpi/SupermanInAction.jpg')";
+console.log("Mouse down on image one");
+};
+
+const revertImageOne = (event) => {
+event.target.style.backgroundImage = "url('resources/img/Me/res-mipmap/mipmap-xxxhdpi/What-I-Look-Like-In-My-Head.jpg')";
+console.log("Mouse up on image one");
+};
+
+const changeImageTwo = (event) => {
+    event.target.style.backgroundImage = "url('resources/img/Playing-Playstation/res-mipmap/mipmap-xxxhdpi/PlayingPlaystation.jpg')";
+    console.log("Mouse down on image two");
+};
+
+const revertImageTwo = (event) => {
+    event.target.style.backgroundImage = "url('resources/img/Actual-Me/res-mipmap/mipmap-xxxhdpi/Actual Me Anon.jpg')";
+    console.log("Mouse up on image two");
+};
+
+imageToChangeOne.addEventListener("mousedown", changeImageOne);
+imageToChangeOne.addEventListener("mouseup", revertImageOne);
+
+imageToChangeTwo.addEventListener("mousedown", changeImageTwo);
+imageToChangeTwo.addEventListener("mouseup", revertImageTwo);

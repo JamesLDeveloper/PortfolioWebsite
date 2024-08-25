44e879384b1e076d80b6e1d14522d91b743d8b9b
JamesLDeveloper <jameslloyd2007@hotmail.co.uk>
2024-08-25 20:18:07 +0100

    do some minor formatting to attributes, remove close button from view while retaining functionality, change mousedown and mousedown event listeners into a toggle image event listener instead

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,63 +1,37 @@
-/*const imagesOne = ["What-I-Think-I-Look-Like-Image"];
-
-const imagesTwo = ["What-I-Actually-Look-Like-Image"];
-
-const imagesOneArray = [];
-
-imagesOne.forEach(function (image) {
-    imagesOneArray.push(document.getElementById(image));
-});
-
-const imagesTwoArray = [];
-
-imagesTwo.forEach(function (image) {
-    imagesTwoArray.push(document.getElementById(image));
-});
-
-imagesOneArray.forEach(function (image) {
-    image.addEventListener("mousedown", changeImageOne);
-    image.addEventListener("mouseup", revertImageOne);
-});
-
-imagesTwoArray.forEach(function (image) {
-    image.addEventListener("mousedown", changeImageTwo);
-    image.addEventListener("mouseup", revertImageTwo);
-});
-*/
-
 const imageToChangeOne = document.getElementById("What-I-Think-I-Look-Like-Image");
 
 const imageToChangeTwo = document.getElementById("What-I-Actually-Look-Like-Image");
 
-const changeImageOne = (event) => {
-event.target.style.backgroundImage = "url('resources/img/Superman-Flying/res-mipmap/mipmap-xxxhdpi/Generic-Superhero-One-In-Action.png')";
-console.log("Mouse down on image one");
-event.target.style.transition = "background-image 0.75s ease-in-out";
-};
 
-const revertImageOne = (event) => {
-event.target.style.backgroundImage = "url('resources/img/Me/res-mipmap/mipmap-xxxhdpi/Generic-Superhero-One.png')";
-console.log("Mouse up on image one");
-event.target.style.transition = "background-image 0.75s ease-in-out";
+let isImageOneChanged = false;
+let isImageTwoChanged = false;
+
+const changeImageOne = (event) => {
+    if (!isImageOneChanged) {
+        event.target.style.backgroundImage = "url('resources/img/Superman-Flying/res-mipmap/mipmap-xxxhdpi/Generic-Superhero-One-In-Action.png')";
+        console.log("Mouse down on image one");
+    } else {
+        event.target.style.backgroundImage = "url('resources/img/Me/res-mipmap/mipmap-xxxhdpi/Generic-Superhero-One.png')";
+        console.log("Mouse up on image one");
+    }
+    event.target.style.transition = "background-image 0.75s ease-in-out";
+    isImageOneChanged = !isImageOneChanged;
 };
 
 const changeImageTwo = (event) => {
-    event.target.style.backgroundImage = "url('resources/img/Playing-Playstation/res-mipmap/mipmap-xxxhdpi/PlayingPlaystation.jpg')";
-    console.log("Mouse down on image two");
+    if (!isImageTwoChanged) {
+        event.target.style.backgroundImage = "url('resources/img/Playing-Playstation/res-mipmap/mipmap-xxxhdpi/PlayingPlaystation.jpg')";
+        console.log("Mouse down on image two");
+    } else {
+        event.target.style.backgroundImage = "url('resources/img/Actual-Me/res-mipmap/mipmap-xxxhdpi/Actual Me Anon.jpg')";
+        console.log("Mouse up on image two");
+    }
     event.target.style.transition = "background-image 0.75s ease-in-out";
+    isImageTwoChanged = !isImageTwoChanged;
 };
 
-const revertImageTwo = (event) => {
-    event.target.style.backgroundImage = "url('resources/img/Actual-Me/res-mipmap/mipmap-xxxhdpi/Actual Me Anon.jpg')";
-    console.log("Mouse up on image two");
-    event.target.style.transition = "background-image 0.75s ease-in-out";
-};
-
-imageToChangeOne.addEventListener("mousedown", changeImageOne);
-imageToChangeOne.addEventListener("mouseup", revertImageOne);
-
-imageToChangeTwo.addEventListener("mousedown", changeImageTwo);
-imageToChangeTwo.addEventListener("mouseup", revertImageTwo);
+imageToChangeOne.addEventListener("click", changeImageOne);
+imageToChangeTwo.addEventListener("click", changeImageTwo);
 
 
 const images = document.querySelectorAll('.hover-enlarge');
